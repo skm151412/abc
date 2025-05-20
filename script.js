b3fa9fddde4c948f1056ccb910f1995910d303bd
skm151412 <skm151412@gmail.com>
2025-05-20 08:34:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -26,6 +26,103 @@ document.addEventListener("DOMContentLoaded", function () {
         q1: "D", // 4.0 g of hydrogen
         q2: "C", // C₂H₄O₃
         q3: "A", // 17.8 M
+        q4: "D", // 7:32
+        q5: "C", // 18 mg
+        q6: "A", // 2.05 M
+        q7: "D", // 1:1:1
+        q8: "D", // CO₂ = 300 mL: CO = 400 mL
+        q9: "C", // 500 K
+        q10: "D", // 2.79 m
+        q11: "A", // 100 mL of 0.2 N HCl
+        q12: "B", // 4.96 g
+        q13: "C", // 14 M
+        q14: "B", // MCl₄
+        q15: "A", // 0.57 M
+        q16: "A", // 0.086
+        q17: "D", // 5.71%, 42.86%
+        q18: "B", // 0.24 g
+        q19: "D", // 14.0%
+        q20: "C", // Pr
+        q21: "B", // 18.1 g
+        q22: "B", // 44% C, 7% H
+        q23: "D", // Temperatures below 0°C are possible in Celsius scale, but in Kelvin scale negative temperature is not possible
+        q24: "A", // 40 mL
+        q25: "A", // 960 g
+        q26: "A", // CH₂O
+        q27: "A", // CH₂O
+        q28: "C", // C₄H₈
+        q29: "A", // H₂
+        q30: "B", // 2
+        q31: "A", // 3.011 × 10²³
+        q32: "A", // 1 g of H₂
+        q33: "A", // 3 × 17 × Avogadro’s number
+        q34: "A", // 90
+        q35: "B", // A = B
+        q36: "A", // 3
+        q37: "B", // <math xmlns="http://www.w3.org/1998/Math/MathML"><semantics><mrow><mfrac><mn>1</mn><mn>8</mn></mfrac></mrow><annotation encoding="application/x-tex">\frac{1}{8}</annotation></semantics></math>
+        q38: "D", // 1
+        q39: "A", // 6
+        q40: "B", // 45
+        q41: "B", // 1
+        q42: "B", // 12
+        q43: "B", // <math xmlns="http://www.w3.org/1998/Math/MathML"><semantics><mrow><mi>x</mi><mo>=</mo><msup><mi>y</mi><mfrac><mn>1</mn><mrow><mi>log</mi><mo>⁡</mo><mn>5</mn></mrow></mfrac></msup></mrow><annotation encoding="application/x-tex">x = y^{\frac{1}{\log 5}}</annotation></semantics></math>
+        q44: "C", // <math xmlns="http://www.w3.org/1998/Math/MathML"><semantics><mrow><msub><mrow><mi>log</mi><mo>⁡</mo></mrow><mn>2</mn></msub><mn>11</mn></mrow><annotation encoding="application/x-tex">\log_2 11</annotation></semantics></math>
+        q45: "B", // 1
+        q46: "B", // 1
+        q47: "C", // 8
+        q48: "A", // <math xmlns="http://www.w3.org/1998/Math/MathML"><semantics><mrow><mo stretchy="false">(</mo><mn>2</mn><mo separator="true">,</mo><mi mathvariant="normal">∞</mi><mo stretchy="false">)</mo></mrow><annotation encoding="application/x-tex">(2, \infty)</annotation></semantics></math>
+        q49: "D", // 4
+        q50: "B", // <math xmlns="http://www.w3.org/1998/Math/MathML"><semantics><mrow><mfrac><mn>2</mn><mrow><mn>2</mn><mo>−</mo><msub><mrow><mi>log</mi><mo>⁡</mo></mrow><mn>2</mn></msub><mn>3</mn></mrow></mfrac></mrow><annotation encoding="application/x-tex">\frac{2}{2 - \log_2 3}</annotation></semantics></math>
+        q51: "A", // 8
+        q52: "B", // 9
+        q53: "B", // 18
+        q54: "B", // 13
+        q55: "C", // 4
+        q56: "B", // 3
+        q57: "C", // 2
+        q58: "B", // 1
+        q59: "C", // 2
+        q60: "C", // 2
+        q61: "C", // 2
+        q62: "A", // -6
+        q63: "B", // 9
+        q64: "C", // 2
+        q65: "C", // 5
+        q66: "B", // 2
+        q67: "A", // <math xmlns="http://www.w3.org/1998/Math/MathML"><semantics><mrow><mo stretchy="false">[</mo><msup><mi mathvariant="normal">L</mi><mn>2</mn></msup><mo stretchy="false">]</mo></mrow><annotation encoding="application/x-tex">[\mathrm{L}^2]</annotation></semantics></math>
+        q68: "B", // <math xmlns="http://www.w3.org/1998/Math/MathML"><semantics><mrow><msub><mi>T</mi><mn>2</mn></msub><mo>=</mo><mfrac><mi>n</mi><mi>m</mi></mfrac><msub><mi>T</mi><mn>1</mn></msub></mrow><annotation encoding="application/x-tex">T_2 = \frac{n}{m} T_1</annotation></semantics></math>
+        q69: "B", // <math xmlns="http://www.w3.org/1998/Math/MathML"><semantics><mrow><mn>1.45</mn><mo>×</mo><mn>1</mn><msup><mn>0</mn><mn>9</mn></msup><mi mathvariant="normal">m</mi></mrow><annotation encoding="application/x-tex">1.45 \times 10^9 \mathrm{m}</annotation></semantics></math>
+        q70: "A", // Ohm
+        q71: "A", // 64
+        q72: "C", // <math xmlns="http://www.w3.org/1998/Math/MathML"><semantics><mrow><msup><mi mathvariant="normal">m</mi><mn>2</mn></msup><msup><mi mathvariant="normal">A</mi><mrow><mo>−</mo><mn>1</mn></mrow></msup><msup><mi mathvariant="normal">s</mi><mrow><mo>−</mo><mn>1</mn></mrow></msup></mrow><annotation encoding="application/x-tex">\mathrm{m^2 A^{-1} s^{-1}}</annotation></semantics></math>
+        q73: "D", // All of the above
+        q74: "A", // A-I, B-III, C-II, D-IV
+        q75: "A", // A-III, B-I, C-II, D-IV
+        q76: "A", // <math xmlns="http://www.w3.org/1998/Math/MathML"><semantics><mrow><mo stretchy="false">[</mo><mrow><mi mathvariant="normal">k</mi><mi mathvariant="normal">g</mi><mi mathvariant="normal">m</mi><msup><mi mathvariant="normal">s</mi><mrow><mo>−</mo><mn>2</mn></mrow></msup></mrow><mo stretchy="false">]</mo></mrow><annotation encoding="application/x-tex">[\mathrm{kg m s^{-2}}]</annotation></semantics></math>
+        q77: "A", // <math xmlns="http://www.w3.org/1998/Math/MathML"><semantics><mrow><mo stretchy="false">[</mo><mi mathvariant="normal">S</mi><mo stretchy="false">]</mo><mo>=</mo><mo stretchy="false">[</mo><mrow><mi mathvariant="normal">M</mi><msup><mi mathvariant="normal">T</mi><mrow><mo>−</mo><mn>2</mn></mrow></msup></mrow><mo stretchy="false">]</mo></mrow><annotation encoding="application/x-tex">[\mathrm{S}] = [\mathrm{M T^{-2}}]</annotation></semantics></math>
+        q78: "C", // <math xmlns="http://www.w3.org/1998/Math/MathML"><semantics><mrow><mo stretchy="false">[</mo><mrow><mi mathvariant="normal">M</mi><msup><mi mathvariant="normal">L</mi><mrow><mo>−</mo><mn>1</mn></mrow></msup><msup><mi mathvariant="normal">T</mi><mrow><mo>−</mo><mn>2</mn></mrow></msup></mrow><mo stretchy="false">]</mo></mrow><annotation encoding="application/x-tex">[\mathrm{M L^{-1} T^{-2}}]</annotation></semantics></math>
+        q79: "B", // <math xmlns="http://www.w3.org/1998/Math/MathML"><semantics><mrow><mo stretchy="false">[</mo><mrow><msup><mi mathvariant="normal">M</mi><mn>0</mn></msup><msup><mi mathvariant="normal">L</mi><mn>0</mn></msup><msup><mi mathvariant="normal">T</mi><mn>0</mn></msup></mrow><mo stretchy="false">]</mo></mrow><annotation encoding="application/x-tex">[\mathrm{M^0 L^0 T^0}]</annotation></semantics></math>
+        q80: "A", // <math xmlns="http://www.w3.org/1998/Math/MathML"><semantics><mrow><mo stretchy="false">[</mo><mrow><msup><mi mathvariant="normal">M</mi><mn>0</mn></msup><msup><mi mathvariant="normal">L</mi><mn>2</mn></msup><msup><mi mathvariant="normal">T</mi><mn>0</mn></msup></mrow><mo stretchy="false">]</mo></mrow><annotation encoding="application/x-tex">[\mathrm{M^0 L^2 T^0}]</annotation></semantics></math>
+        q81: "A", // <math xmlns="http://www.w3.org/1998/Math/MathML"><semantics><mrow><mo stretchy="false">[</mo><mi>P</mi><msup><mi>A</mi><mrow><mo>−</mo><mn>1</mn></mrow></msup><mi>T</mi><mo stretchy="false">]</mo></mrow><annotation encoding="application/x-tex">[P A^{-1} T]</annotation></semantics></math>
+        q82: "D", // Velocity gradient and decay constant
+        q83: "A", // <math xmlns="http://www.w3.org/1998/Math/MathML"><semantics><mrow><mo stretchy="false">[</mo><mi>P</mi><mi>V</mi><mo stretchy="false">]</mo></mrow><annotation encoding="application/x-tex">[P V]</annotation></semantics></math>
+        q84: "D", // <math xmlns="http://www.w3.org/1998/Math/MathML"><semantics><mrow><mi>L</mi><mi mathvariant="normal">/</mi><mi>C</mi></mrow><annotation encoding="application/x-tex">L/C</annotation></semantics></math>
+        q85: "A", // <math xmlns="http://www.w3.org/1998/Math/MathML"><semantics><mrow><mo stretchy="false">[</mo><mrow><mi mathvariant="normal">M</mi><msup><mi mathvariant="normal">L</mi><mrow><mo>−</mo><mn>1</mn></mrow></msup><msup><mi mathvariant="normal">T</mi><mrow><mo>−</mo><mn>1</mn></mrow></msup></mrow><mo stretchy="false">]</mo></mrow><annotation encoding="application/x-tex">[\mathrm{M L^{-1} T^{-1}}]</annotation></semantics></math>
+        q86: "C", // <math xmlns="http://www.w3.org/1998/Math/MathML"><semantics><mrow><mo stretchy="false">[</mo><mrow><msup><mi mathvariant="normal">M</mi><mn>0</mn></msup><msup><mi mathvariant="normal">L</mi><mrow><mo>−</mo><mn>1</mn></mrow></msup><msup><mi mathvariant="normal">T</mi><mn>0</mn></msup></mrow><mo stretchy="false">]</mo></mrow><annotation encoding="application/x-tex">[\mathrm{M^0 L^{-1} T^0}]</annotation></semantics></math>
+        q87: "A", // <math xmlns="http://www.w3.org/1998/Math/MathML"><semantics><mrow><mo stretchy="false">[</mo><mrow><mi mathvariant="normal">M</mi><msup><mi mathvariant="normal">L</mi><mn>2</mn></msup><msup><mi mathvariant="normal">T</mi><mrow><mo>−</mo><mn>2</mn></mrow></msup><msup><mi mathvariant="normal">A</mi><mrow><mo>−</mo><mn>2</mn></mrow></msup></mrow><mo stretchy="false">]</mo></mrow><annotation encoding="application/x-tex">[\mathrm{M L^2 T^{-2} A^{-2}}]</annotation></semantics></math>
+        q88: "D", // Dipole moment and electric flux
+        q89: "C", // <math xmlns="http://www.w3.org/1998/Math/MathML"><semantics><mrow><mi>J</mi><mo>=</mo><mi>ϵ</mi><mfrac><mrow><mi mathvariant="normal">∂</mi><mi>E</mi></mrow><mrow><mi mathvariant="normal">∂</mi><mi>t</mi></mrow></mfrac></mrow><annotation encoding="application/x-tex">J = \epsilon \frac{\partial E}{\partial t}</annotation></semantics></math>
+        q90: "C", // Permeability of free space
+        q91: "A", // <math xmlns="http://www.w3.org/1998/Math/MathML"><semantics><mrow><mo stretchy="false">[</mo><mrow><mi mathvariant="normal">F</mi><msup><mi mathvariant="normal">L</mi><mrow><mo>−</mo><mn>4</mn></mrow></msup><msup><mi mathvariant="normal">T</mi><mn>2</mn></msup></mrow><mo stretchy="false">]</mo></mrow><annotation encoding="application/x-tex">[\mathrm{F L^{-4} T^2}]</annotation></semantics></math>
+        q92: "A", // <math xmlns="http://www.w3.org/1998/Math/MathML"><semantics><mrow><mo stretchy="false">[</mo><mrow><msup><mi mathvariant="normal">M</mi><mn>0</mn></msup><msup><mi mathvariant="normal">L</mi><mn>0</mn></msup><msup><mi mathvariant="normal">T</mi><mn>0</mn></msup></mrow><mo stretchy="false">]</mo></mrow><annotation encoding="application/x-tex">[\mathrm{M^0 L^0 T^0}]</annotation></semantics></math>
+        q93: "A", // <math xmlns="http://www.w3.org/1998/Math/MathML"><semantics><mrow><mo stretchy="false">[</mo><mrow><msup><mi mathvariant="normal">M</mi><mn>0</mn></msup><mi mathvariant="normal">L</mi><msup><mi mathvariant="normal">T</mi><mrow><mo>−</mo><mn>1</mn></mrow></msup></mrow><mo stretchy="false">]</mo></mrow><annotation encoding="application/x-tex">[\mathrm{M^0 L T^{-1}}]</annotation></semantics></math>
+        q94: "A", // <math xmlns="http://www.w3.org/1998/Math/MathML"><semantics><mrow><mo stretchy="false">[</mo><mrow><msup><mi mathvariant="normal">M</mi><mn>0</mn></msup><msup><mi mathvariant="normal">L</mi><mn>0</mn></msup><msup><mi mathvariant="normal">T</mi><mn>0</mn></msup></mrow><mo stretchy="false">]</mo></mrow><annotation encoding="application/x-tex">[\mathrm{M^0 L^0 T^0}]</annotation></semantics></math>
+        q95: "B", // <math xmlns="http://www.w3.org/1998/Math/MathML"><semantics><mrow><mo stretchy="false">[</mo><mrow><msup><mi mathvariant="normal">M</mi><mn>0</mn></msup><mi mathvariant="normal">L</mi><msup><mi mathvariant="normal">T</mi><mn>0</mn></msup></mrow><mo stretchy="false">]</mo></mrow><annotation encoding="application/x-tex">[\mathrm{M^0 L T^0}]</annotation></semantics></math>
+        q96: "A", // Same dimensions for <math xmlns="http://www.w3.org/1998/Math/MathML"><semantics><mrow><mi>x</mi><mo separator="true">,</mo><mi>y</mi><mo separator="true">,</mo><mi>z</mi></mrow><annotation encoding="application/x-tex">x, y, z</annotation></semantics></math>
+        q97: "A", // <math xmlns="http://www.w3.org/1998/Math/MathML"><semantics><mrow><mo stretchy="false">[</mo><mrow><mi mathvariant="normal">M</mi><mi mathvariant="normal">L</mi><msup><mi mathvariant="normal">T</mi><mrow><mo>−</mo><mn>3</mn></mrow></msup><msup><mi mathvariant="normal">K</mi><mrow><mo>−</mo><mn>1</mn></mrow></msup></mrow><mo stretchy="false">]</mo></mrow><annotation encoding="application/x-tex">[\mathrm{M L T^{-3} K^{-1}}]</annotation></semantics></math>
+        q98: "C", // <math xmlns="http://www.w3.org/1998/Math/MathML"><semantics><mrow><mo stretchy="false">[</mo><mrow><mi mathvariant="normal">M</mi><msup><mi mathvariant="normal">L</mi><mn>0</mn></msup><msup><mi mathvariant="normal">T</mi><mrow><mo>−</mo><mn>3</mn></mrow></msup></mrow><mo stretchy="false">]</mo></mrow><annotation encoding="application/x-tex">[\mathrm{M L^0 T^{-3}}]</annotation></semantics></math>
+        q99: "A", // <math xmlns="http://www.w3.org/1998/Math/MathML"><semantics><mrow><mo stretchy="false">[</mo><mi>P</mi><msup><mi>A</mi><mrow><mn>1</mn><mi mathvariant="normal">/</mi><mn>2</mn></mrow></msup><msup><mi>T</mi><mrow><mo>−</mo><mn>1</mn></mrow></msup><mo stretchy="false">]</mo></mrow><annotation encoding="application/x-tex">[P A^{1/2} T^{-1}]</annotation></semantics></math>
+        q100: "A", // <math xmlns="http://www.w3.org/1998/Math/MathML"><semantics><mrow><msqrt><mfrac><msub><mi>μ</mi><mn>0</mn></msub><msub><mi>ϵ</mi><mn>0</mn></msub></mfrac></msqrt></mrow><annotation encoding="application/x-tex">\sqrt{\frac{\mu_0}{\epsilon_0}}</annotation></semantics></math>
         
     };
 
